71d8771db8cb923e185ad8689b7faf597e05edab
GitHub Action <action@github.com>
2021-12-23 20:20:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/cesarrichard/unicornprogressbar/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.cesarrichard.unicornprogressbar.services
+
+import com.github.cesarrichard.unicornprogressbar.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
